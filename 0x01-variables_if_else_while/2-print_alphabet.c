2c66e933b51675988876099957213f76d57efcb3
kevkatam <kevinkipruto5077@gmail.com>
2023-03-29 08:41:19 -0700

    added 2-print_alphabet.c, updated README.md

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alph[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i = 0;
+
+	for(i = 0; i < 26; i++)
+	{
+		putchar(alph[i]);
+	}
+	putchar('\n');
+	return (0);
+}
