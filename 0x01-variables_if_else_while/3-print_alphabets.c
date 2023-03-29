24cd06b6817c493c60392789d8c2123ae6ae9124
kevkatam <kevinkipruto5077@gmail.com>
2023-03-29 09:05:29 -0700

    modified 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,12 +4,12 @@
  *
  * Return: Always 0 (Success)
  */
-int main (void)
+int main(void)
 {
 	char alph[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
 
-	for (i = 0; i <52; i++)
+	for (i = 0; i < 52; i++)
 	{
 		putchar(alph[i]);
 	}
