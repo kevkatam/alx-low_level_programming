decd4976a23fdd5b41836c558795993ca82a1b1e
kevkatam <kevinkipruto5077@gmail.com>
2023-05-23 10:25:54 -0700

    modified 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,12 +8,13 @@ void print_alphabet_x10(void)
 	int i;
 	int a;
 
-	for(a = 0; a <= 9; a++)
+	for (a = 0; a <= 9; a++)
 	{
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
 		}
+
 	_putchar('\n');	
 	}
 }
