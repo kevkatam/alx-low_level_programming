1a9510cdb751744b7c7556d7e5c72b3eb5861f6c
kevkatam <kevinkipruto5077@gmail.com>
2023-05-24 00:58:29 -0700

    added 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - prints every minute of the day
+ * Return: no return
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	for (i>= 48; i <= 50; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = 48; k <= 53; k++)
+			{
+				for (l =48; l <= 57; l++)
+				{
+					if(i >= 50 && j >= 52)
+						break;
+					_putchar(i);
+					_putchar(j);
+					_putchar(58);
+					_putchar(k);
+					_putchar(l);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
