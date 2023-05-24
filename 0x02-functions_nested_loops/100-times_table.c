62fc4baae56ed73258971c45da4208b0224636f0
kevkatam <kevinkipruto5077@gmail.com>
2023-05-24 09:26:15 -0700

    modified 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,30 +13,29 @@ void print_times_table(int n)
 		for (a = 0; a <= n; a++)
 		{
 			_putchar(48);
-		}
-		for (b = 1; b <= n; b++)
-		{
-			c = a * b;
-			_putchar(44);
-			_putchar(32);
-			if (c <= 9)
+			for (b = 1; b <= n; b++)
 			{
+				c = a * b;
+				_putchar(44);
 				_putchar(32);
-				_putchar(c + 48);
+				if (c <= 9)
+				{
+					_putchar(32);
+					_putchar(c + 48);
+				}
+				else if (c <= 99)
+				{
+					_putchar(32);
+					_putchar(c / 10 + 48);
+					_putchar(c % 10 + 48);
+				}
+				else
+				{
+					_putchar((c / 100) % 10 + 48);
+					_putchar((c / 10) % 10 + 48);
+					_putchar(c % 10 + 48);
+				}
 			}
-			else if (c <= 99)
-			{
-				_putchar(32);
-				_putchar(c / 10 + 48);
-				_putchar(c % 10 + 48);
-			}
-			else
-			{
-				_putchar((c / 100) % 10 + 48);
-				_putchar((c / 10) % 10 + 48);
-				_putchar(c % 10 + 48);
-			}
-		}
-		_putchar('\n');
+		}	_putchar('\n');
 	}
 }
