1d5895c9fed09cacb4e6d7793804356d19c4c868
kevkatam <kevinkipruto5077@gmail.com>
2023-05-29 07:50:15 -0700

    modified 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,14 @@
  */
 void print_rev(char *s)
 {
-	int count = _strlen(s);
+	int count = 0;
 
-	while (count >= 0 )
+	while (*(s + count) = '\0')
+	{
+		count++;
+	}
+
+	while (count >= 0)
 	{
 		_putchar(s[count]);
 		count--;
