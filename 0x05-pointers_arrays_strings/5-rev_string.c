885381f52f3e6b73458671a47b0e4dbe4dd7daeb
kevkatam <kevinkipruto5077@gmail.com>
2023-05-29 11:03:00 -0700

    modified 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,20 @@
  */
 void rev_string(char *s)
 {
-	int i, count = 0;
+	int count = 0;
 
 	while (s[count] != '\0')
 	{
-		_putchar(s[count]);	
+		_putchar(s[count]);
 		count++;
 	}
 	_putchar('\n');
-	i = count - 1;
+	count = count - 1;
 
-	while (i >= 0)
+	while (count >= 0)
 	{
-		_putchar(s[i]);
-		i--;
+		_putchar(s[count]);
+		count--;
 	}
 	_putchar('\n');
 }
