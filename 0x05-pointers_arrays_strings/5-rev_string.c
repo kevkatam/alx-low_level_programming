82971dc7592c25f8c489989d6fb9644e4d491931
kevkatam <kevinkipruto5077@gmail.com>
2023-05-29 14:54:49 -0700

    modified 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,16 +6,24 @@
  */
 void rev_string(char *s)
 {
-	int count = 0;
+	int count = 0, a, b;
+	char *str, temp;
 
-	while (s[count] != '\0')
+	while (count >= 0)
 	{
+		if (s[count] == '\0')
+			break;
 		count++;
-		
-		count = count -1;
-		while (count >= 0)
+	}
+	str = s;
+
+	for (a = 0; a < (count - 1); a++)
+	{
+		for (b = a + 1; b > 0; b--)
 		{
-			count--;
+			temp = *(str + b);
+			*(str + b) = *(str + (b - 1));
+			*(str + (b - 1)) = temp;
 		}
 	}
 }
