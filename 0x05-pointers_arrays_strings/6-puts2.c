2c2594ea29ec52e5fc4c63daa9724ba7ddb9885f
kevkatam <kevinkipruto5077@gmail.com>
2023-05-30 02:05:34 -0700

    modified 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,17 +8,13 @@ void puts2(char *str)
 {
 	int count = 0;
 
-	while (count >= 0)
+	while (count != '\0')
 	{
-		if (count == '\0')
-		{
-			_putchar('\n');
-			break;
-		}
 		if (count % 2 == 0)
 		{
 			_putchar(str[count]);
-		count++;
+			count++;
 		}
 	}
+	_putchar('\n');
 }
