5e740957aee0f86c093e35293e32f4e0aa85c71b
kevkatam <kevinkipruto5077@gmail.com>
2023-06-01 07:38:55 -0700

    added 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: input string
+ * Return: uppercase of a string
+ */
+char *string_toupper(char *str)
+{
+	int count = 0;
+
+	while (*(str + count) != '\0')
+	{
+		if ((*(str + count) >= 97) && (*(str + count) <= 122))
+			*(str + count) = *(str + count) - 32;
+		count++;
+	}
+	return (str);
+}
