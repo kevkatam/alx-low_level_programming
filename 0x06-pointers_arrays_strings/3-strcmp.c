c73f7df3343bb62e0dcdc5c49ba01af33a032fdd
kevkatam <kevinkipruto5077@gmail.com>
2023-06-01 06:57:46 -0700

    modified 2-strncpy.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,7 +13,7 @@ int _strcmp (char *s1, char *s2)
 	{
 		if ((*(s1 + a) == '\0') && (*(s2 + a) == '\0'))
 			break;
-		b = (*(s1 + a)) - (*(s2 + a));
+		b = *(s1 + a) - *(s2 + a);
 		a++;
 	}
 	return (b);
