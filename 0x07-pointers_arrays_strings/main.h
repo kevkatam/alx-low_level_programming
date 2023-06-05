b9935523d727bc905c54eb04d5b5cbce64257460
kevkatam <kevinkipruto5077@gmail.com>
2023-06-05 12:35:27 -0700

    added 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -7,5 +7,7 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+char *_strstr(char *haystack, char *needle);
+int _putchar(char);
 
 #endif
