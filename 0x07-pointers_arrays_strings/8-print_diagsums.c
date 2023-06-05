080617c01d0217058953855969543db2a2e3f893
kevkatam <kevinkipruto5077@gmail.com>
2023-06-05 13:41:33 -0700

    modified 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -5,7 +5,7 @@
  * a square matrix of integers
  * @a: input pointer
  * @size: matrix size
- * Return: nothing
+ * Return: no return
  */
 void print_diagsums(int *a, int size)
 {
@@ -15,7 +15,7 @@ void print_diagsums(int *a, int size)
 	{
 		if (b % (size + 1) == 0)
 			sum1 += *(a +1);
-		if (b % (size - 1) == 0 && b != 0&& b < size * size - 1)
+		if (b % (size - 1) == 0 && b != 0 && b < size * size - 1)
 			sum2 += *(a + b);
 	}
 	printf("%d, %d\n", sum1, sum2);
