1ee99957987e393644c97d54d34fa8b4ecf54490
kevkatam <kevinkipruto5077@gmail.com>
2023-06-06 00:41:05 -0700

    added 100-set_string.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -11,5 +11,6 @@ char *_strstr(char *haystack, char *needle);
 int _putchar(char);
 void set_string(char **s, char *to);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 
 #endif
