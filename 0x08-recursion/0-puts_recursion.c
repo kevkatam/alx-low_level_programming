ea0ef18ad64039c7fa1778a2d89f3edab85bf559
kevkatam <kevinkipruto5077@gmail.com>
2023-06-07 03:22:45 -0700

    modified 0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i;
-
-	if (*(s + i) == 0)
-		return (s);
+	if (*s != '\0')
 	{
-		_putchar(*(s + i));
-		_puts_recursion(*(s + i - 1));
-	i++;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar('\n');
+	else
+		_putchar('\n');
 }
-
