aee64490d6d4d442016e0a1151d270ab5b73659d
kevkatam <kevinkipruto5077@gmail.com>
2023-06-15 08:32:40 -0700

    added 4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,12 +10,11 @@
  */
 void free_grid(int **grid, int height)
 {
-	int i;
 
 	if (grid != NULL && height != 0)
 	{
-		for (i = 0; i < height; i--)
-			free(grid[i]);
+		for (; height >= 0; height--)
+			free(grid[height]);
 		free(grid);
 	}
 }
