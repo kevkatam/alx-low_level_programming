343a75f30073a4881dafbf152d09bc72ad447098
kevkatam <kevinkipruto5077@gmail.com>
2023-06-18 04:42:15 -0700

    modified 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -16,7 +16,7 @@ int *array_range(int min, int max)
 		return (NULL);
 	}
 
-	a = malloc(sizeof(*a) * ((min - max) + 1));
+	a = malloc(sizeof(*a) * ((max - min) + 1));
 
 	if (a == NULL)
 		return (NULL);
