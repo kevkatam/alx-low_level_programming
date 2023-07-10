5ad3432df6804bfd2dab31be086a8975c64f04d2
kevkatam <kevinkipruto5077@gmail.com>
2023-07-10 10:27:13 -0700

    added 3-cp.c

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,69 @@
+#include "main.h"
+/**
+ * err_file - checks if the permissions of the files arre allowed
+ * @file_from: file_from
+ * @file_to: file_to
+ * @argv: argument vector
+ * Return: no return
+ */
+void err_file(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(98);
+	}
+}
+/**
+ * main - a program that copies the content of a file to another file.
+ * @argc: number of command lin arguments
+ * @argv: argument vector
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int file_from, file_to, err_close;
+	ssize_t n, wr;
+	char buf[1024];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 664);
+
+	err_file(file_from, file_to, argv);
+
+	n = 1024;
+	while (n == 1024)
+	{
+		n = read(file_from, buf, 1024);
+		if (n == -1)
+			err_file(-1, 0, argv);
+		wr = write(file_to, buf, 1024);
+		if (wr == -1)
+			err_file(0, -1, argv);
+	}
+	err_close = close(file_from);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	err_close = close(file_to);
+	if (err_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+	return (0);
+}
+
+
