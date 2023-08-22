4b00b45046f03de5e7fc4374571e3df7fef6e428
kevkatam <kevinkipruto5077@gmail.com>
2023-08-22 14:57:55 -0700

    modified 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/ https://raw.github.com/kevkatam/alx-low_level_programming/master/0x18-dynamic_librarie/nrandom.so
+wget -P /tmp/ https://raw.github.com/kevkatam/alx-low_level_programming/main/0x18-dynamic_librarie/nrandom.so
 export LD_PRELOAD=/tmp/nrandom.so
